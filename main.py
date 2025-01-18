856e0dd2ce7c5f76adf47349bfd9cf7ed4b29b39
Hamozwa <iamhanimoussa@gmail.com>
2025-01-18 23:22:49 +0000

    Add alternative method

--- main.py
+++ main.py
@@ -22,12 +22,24 @@ polynomial = np.poly1d(coefficients)
 train_poly_values = polynomial(train_days)
 validation_poly_values = polynomial(validation_days)
 
-# Fit a polynomial to the training data using manual method
-degree_manual = 7
-X_train = np.vander(train_days, degree_manual)
-coefficients_manual = np.linalg.inv(X_train.T @ X_train) @ X_train.T @ train_data['Close']
-train_poly_values_manual = X_train @ coefficients_manual
-validation_poly_values_manual = np.vander(validation_days, degree_manual) @ coefficients_manual
+# Standardize the input data
+mean_train_days = np.mean(train_days)
+std_train_days = np.std(train_days)
+train_days_standardized = (train_days - mean_train_days) / std_train_days
+validation_days_standardized = (validation_days - mean_train_days) / std_train_days
+
+# Fit a polynomial to the training data manually
+degree = 6  # Degree of the polynomial
+X_train = np.vander(train_days_standardized, degree + 1)
+y_train = train_data['Close'].values
+
+# Solve for the polynomial coefficients using the normal equation
+coefficients = np.linalg.inv(X_train.T @ X_train) @ X_train.T @ y_train
+
+# Generate polynomial values for training and validation data
+train_poly_values = X_train @ coefficients
+X_validation = np.vander(validation_days_standardized, degree + 1)
+validation_poly_values = X_validation @ coefficients
 
 # Plot the data and the polynomial fits using numpy
 plt.figure(figsize=(10, 5))
@@ -45,10 +57,10 @@ plt.show()
 plt.figure(figsize=(10, 5))
 plt.plot(train_data.index, train_data['Close'], label='Training Data')
 plt.plot(validation_data.index, validation_data['Close'], label='Validation Data')
-plt.plot(train_data.index, train_poly_values_manual, label='Polynomial Fit (manual)', linestyle='--')
-plt.plot(validation_data.index, validation_poly_values_manual, label='Polynomial Fit (manual)', linestyle='--')
+plt.plot(train_data.index, train_poly_values, label='Polynomial Fit', linestyle='--')
+plt.plot(validation_data.index, validation_poly_values, label='Polynomial Fit', linestyle='--')
 plt.xlabel('Date')
 plt.ylabel('Close Price')
-plt.title('MSFT Closing Prices - Polynomial Fit (manual)')
+plt.title('MSFT Closing Prices - Polynomial Fit')
 plt.legend()
 plt.show()
